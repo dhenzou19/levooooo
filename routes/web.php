b1ba27c1fefc96a48dcd98c57f5ef1f31f3e1b84
dhenzou19 <dhenmarctagsip@gmail.com>
2026-01-07 21:07:40 +0800

    this for new pages

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/aboutpage', function () {
+    return view('aboutpage');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
